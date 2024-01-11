1321845f7e7fbddb4209f9b084e1dec36a5889cb
佐藤　文彦 <fumihiko_sato@post.pioneer.co.jp>
2024-01-11 22:40:12 +0900

    Figmaプラグイン

---
+++ code.tsx
@@ -0,0 +1,92 @@
+figma.showUI(__html__, { width: 400, height: 220 });
+
+figma.on("run", ({ parameters }: RunEvent) => {
+  const iosSuffix = ["", "@2x", "@3x"];
+  const androidSuffix = ["ldpi", "mdpi", "hdpi", "xhdpi", "xxhdpi", "xxxhdpi"];
+  const androidScale = [0.75, 1, 1.5, 2, 3, 4];
+
+  type FormatType = "JPG" | "PNG" | "SVG" | "PDF";
+  type NodesType = readonly SceneNode[];
+
+  const exportImages = async (
+    nodes: NodesType,
+    format: FormatType,
+    platform: string[]
+  ) => {
+    try {
+      const imagesPromise = nodes
+        .map((node) => {
+          let iOSImages: Promise<Uint8Array>[] = [];
+          let androidImages: Promise<Uint8Array>[] = [];
+          const width = node.width;
+          console.log(platform);
+          if (platform.find((p) => p === "iOS")) {
+            iOSImages = iosSuffix.map((suffix, index) =>
+              node.exportAsync({
+                format,
+                constraint: { type: "SCALE", value: index + 1 },
+              })
+            );
+          }
+          if (platform.find((p) => p === "Android")) {
+            androidImages = androidSuffix.map((suffix, index) => {
+              return node.exportAsync({
+                format,
+                constraint: {
+                  type: "SCALE",
+                  value: androidScale[index],
+                },
+              });
+            });
+          }
+          console.log([...iOSImages, ...androidImages]);
+          return [...iOSImages, ...androidImages];
+        })
+        .flat();
+
+      const names = nodes
+        .map((node) => [
+          // 後でファイル名からプラットフォームを識別するために_IOS_など利用されなそうな文字列を付与する
+          ...iosSuffix.map((suffix) => `_IOS_${node.name}${suffix}`),
+          ...androidSuffix.map((suffix) => `_ANDROID_${node.name}_${suffix}`),
+        ])
+        .flat();
+
+      //node.nameに重複がある場合はエラーを返す
+      if (hasDuplicateValue(names)) {
+        figma.ui.postMessage({ type: "hasDuplicateValue" });
+        return;
+      }
+      const images = await Promise.all(imagesPromise);
+      return { images, names, format: format.toLowerCase() };
+    } catch (error) {
+      console.error("画像の書き出しエラー", error);
+      throw error;
+    }
+
+    function hasDuplicateValue(array: string[]) {
+      const set = new Set(array);
+      return array.length !== set.size;
+    }
+  };
+
+  figma.ui.onmessage = async (message) => {
+    if (message.type === "export") {
+      const nodes = figma.currentPage.selection;
+      if (nodes.length > 0) {
+        try {
+          const result = await exportImages(
+            nodes,
+            message.format,
+            message.platform
+          );
+          figma.ui.postMessage({ type: "exportImage", ...result });
+        } catch (error) {
+          figma.ui.postMessage({ type: "exportError", error });
+        }
+      } else {
+        figma.ui.postMessage({ type: "unselectedNode" });
+      }
+    }
+  };
+});
